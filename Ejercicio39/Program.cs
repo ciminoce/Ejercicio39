6f2d0d49dc06f47be7d58f4576cfbebcf823e8af
ciminoce <ciminoce@yahoo.com.ar>
2021-06-14 19:49:43 -0300

    Ejercicio completo

--- Ejercicio39/Program.cs
+++ Ejercicio39/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,88 @@ namespace Ejercicio39
     {
         static void Main(string[] args)
         {
+            try
+            {
+                double celsius, conversion;
+                int opcion;
+                Console.Clear();
+                Console.WriteLine("Conversión de temperaturas Celsius");
+                Console.Write("Ingrese la temperatura en grados celsius:");
+                celsius = Convert.ToDouble(Console.ReadLine());
+                if (celsius > 15)
+                {
+                    Console.WriteLine("1-Fahrenheit");
+                    Console.WriteLine("2-Reaumur");
+                    Console.WriteLine("3-Kelvin");
+                    Console.WriteLine("4-Rankine");
+                    Console.Write("Ingrese la opción seleccionada:");
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                    if (opcion >= 1 && opcion <= 4)
+                    {
+                        switch (opcion)
+                        {
+                            case 1:
+                                conversion = ConvertiraFahrenheit(celsius);
+                                Console.WriteLine($"La conversión a Fah es {conversion}");
+                                break;
+                            case 2:
+                                conversion = ConvertiraReaumur(celsius);
+                                Console.WriteLine($"La conversión a Reaumur es {conversion}");
+                                break;
+                            case 3:
+                                conversion = ConvertiraKelvin(celsius);
+                                Console.WriteLine($"La conversión a Kelvin es {conversion}");
+                                break;
+                            default:
+                                conversion = ConvertiraRankine(celsius);
+                                Console.WriteLine($"La conversión a Rankine es {conversion}");
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Opción no válida");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("la temperatura debe ser superior a los 15 grados");
+                }
+
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Número mal ingresado... fin del programa");
+
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Número demasiado grande...Fin del programa");
+            }
+            Console.ReadLine();
+        }
+
+        private static double ConvertiraRankine(double celsius)
+        {
+            return 1.8 * celsius + 491.67;
+        }
+
+        private static double ConvertiraKelvin(double celsius)
+        {
+            return celsius + 273.15;
+        }
+
+        private static double ConvertiraReaumur(double celsius)
+        {
+            return 0.8 * celsius;
+        }
+
+        private static double ConvertiraFahrenheit(double celsius)
+        {
+            return 1.8 * celsius + 32;
         }
     }
 }
